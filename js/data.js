185880d4d3fab293ce6bf6a635f760a96782d770
wihyanghoon <gidgns1995@gmail.com>
2023-02-11 18:59:44 +0900

    fix data

--- js/data.js
+++ js/data.js
@@ -2,13 +2,13 @@ const projectList = [
     {
         id: 1,
         No: 1,
-        title: '포트폴리오사이트 - The PhotoShop',
+        title: '리액트프로젝트 - Moobpl',
         img: [
             "./img/preject/photoshop01.jpg",
             "./img/preject/photoshop02.jpg",
         ],
         mainImg: "./img/preject/photoshop.jpg",
-        stack: "Html/CSS, Javascript",
+        stack: "React.js, express.js",
         date: "2022.11 ~ 2022.12",
         desc: "디자인 및 기능개발",
         explanation: {
@@ -16,96 +16,6 @@ const projectList = [
             function: `배열을 통해서 데이터를 뿌려주고 배열 정렬을 통해 작품을 번호, 제목, 날짜 등으로 재정렬하여 출력할 수 있는 기능을 만들었습니다.
             또한, URL의 쿼리값을 통하여 상세페이지의 데이터를 불러오도록 제작하였습니다.`
         }
-    },
-    {
-        id: 12312321,
-        No: 2,
-        title: '수자원공사 - 물, 자연 그리고 사람',
-        img: [
-            "./img/preject/k-water01.jpg",
-            "./img/preject/k-water02.jpg",
-        ],
-        mainImg: "./img/preject/k-water.jpg",
-        stack: "Html/CSS, Javscript, jQerry",
-        date: "2022.01 ~ 2022.08",
-        desc: "사이트 개설,제작 및 유지보수, 통계관리",
-        url: "http://www.k-waterwebzine.com/"
-    }, {
-        id: 12312312,
-        No: 3,
-        title: '대한주택건설협회 - 주택플러스',
-        img: [
-            "./img/preject/plus-h01.jpg",
-            "./img/preject/plus-h02.jpg",
-        ],
-        mainImg: "./img/preject/plus-h.jpg",
-        stack: "Html/CSS, Javscript, jQerry",
-        date: "2021.04 ~ 2022.03",
-        desc: "유지보수 및 통계관리",
-        url: "http://www.plus-h.co.kr/"
-    }, {
-        id: 23123123123,
-        No: 4,
-        title: '하이원리조트 - 1340',
-        img: [
-            "./img/preject/1340-01.jpg",
-            "./img/preject/1340-02.jpg",
-        ],
-        mainImg: "./img/preject/1340.jpg",
-        stack: "Html/CSS, Javscript, jQerry",
-        date: "2021.08 ~ 2021.12",
-        desc: "유지보수 및 통계관리",
-        url: "http://1340webzine.com/data/211112/main.php"
-    },
-    {
-        id: 31235465676787,
-        No: 5,
-        title: 'LH 한국토지주택공사 - withLH',
-        img: [
-            "./img/preject/lh01.jpg",
-            "./img/preject/lh02.jpg",
-        ],
-        mainImg: "./img/preject/lh.jpg",
-        stack: "Html/CSS, Javscript, jQerry",
-        date: "2021.04 ~ 2021.12",
-        desc: "유지보수 및 통계관리",
-        url: "http://webzine.lh.or.kr/"
-    }, {
-        id: 4557657654543,
-        No: 6,
-        title: '한국남동발전 - 남동愛',
-        img: [
-            "./img/preject/koen01.jpg",
-            "./img/preject/koen02.jpg",
-        ],
-        mainImg: "./img/preject/koen.jpg",
-        stack: "Html/CSS, Javscript, jQerry",
-        date: "2021.04 ~ 2021.12",
-        desc: "유지보수 및 통계관리",
-    }, {
-        id: 45454545231175675,
-        No: 7,
-        title: '토이프로젝트 - Mctavish 리뉴얼',
-        img: [
-            "./img/preject/mctavish.jpg",
-            "./img/preject/project-ducati2.jpg",
-        ],
-        mainImg: "./img/preject/mctavish.jpg",
-        stack: "html/css, Javscript, jQerry",
-        date: "2020.04 ~ 2020.05",
-        desc: "디자인 및 퍼블리싱",
-    }, {
-        id: 7872235532333454,
-        No: 8,
-        title: '토이프로젝트 - Ducati 리뉴얼',
-        img: [
-            "./img/preject/ducati.jpg",
-            "./img/preject/project-ducati2.jpg",
-        ],
-        mainImg: "./img/preject/ducati.jpg",
-        stack: "html/css, Javscript, jQerry",
-        date: "2020.04 ~ 2020.05",
-        desc: "디자인 및 퍼블리싱",
     },
     {
         id: 1,
