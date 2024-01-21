3bc961f7fc4768d4bcd09f817088fe2a7c38e75a
wihyanghoon <gidgns1995@gmail.com>
2024-01-22 04:22:21 +0900

    [modify] skill img

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ const projectList = [
     {
         id: 5656765,
         No: 1,
-        title: 'CJ - CJ그룹 사이트',
+        title: 'CJ - CJ그룹 웹사이트',
         img: [
             "./img/preject/moobpl01.jpg",
             "./img/preject/moobpl02.jpg",
@@ -21,20 +21,21 @@ const projectList = [
     {
         id: 5656765,
         No: 1,
-        title: 'SAMSUNG - SmartThings-IFA explore',
+        title: 'SmartThings - IFA explore',
         img: [
             "./img/preject/moobpl01.jpg",
             "./img/preject/moobpl02.jpg",
         ],
         mainImg: "./img/preject/moobpl.jpg",
-        stack: "EJS, Typescript, SCSS",
-        date: "2023.07.10 ~ 2024.01.19",
-        desc: "프론트엔드 개발, 검색기능 및 동영샹 view, 국가별 redirection, 연관검색어 하이라이팅",
+        stack: "Gatsby, GraphQL, Typescript, MDX, SCSS",
+        date: "2023.07.24 ~ 2023.11.03",
+        desc: "프론트엔드 개발, 구글애널리틱스",
         url:"https://www.cj.net/",
         explanation: {
-            info: `회사 프로젝트로 CJ그룹사이트 제작 참여, 검색 기능 및 동영상 view 기능을 개발`,
-            function: `API 처리 및 검색 기능개발, 연관검색어 하이라이팅 및 검색어에 따른 조건부 랜더링, 구글애널리틱스 커스텀이벤트, 국가별 리다이렉션`,
-            stack: "EJS, Typescript, SCSS",
+            info: `Sustainability경험을 포함한 SmartThings 경험 스토리를 소개하고 추천하도록 목업을 제작하고,
+            이를 한국총괄 프로모션, SmartThings.com 사이트, 삼성 닷컴 등에 적용 IFA 2023 삼성 스마트싱스 행사장에서 사용되고 후에 스마트싱스 사이트 및 삼성전자 사이트에서도 활용됨.`,
+            function: `조건에 따른 데이터 출력 및 상태관리`,
+            stack: "Gatsby, GraphQL, Typescript, MDX, SCSS",
         }
     },
     {
@@ -229,3 +230,10 @@ const projectList = [
         }
     }
 ]
+
+projectList.forEach((preject , index)=>{
+    preject.id = index;
+    preject.No = index;
+})
+
+console.log(projectList)
